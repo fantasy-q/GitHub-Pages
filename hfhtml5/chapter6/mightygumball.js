4bea7924b06e805a4c80430f13517be55d8589bb
fantasy-q <fantasy-q@users.noreply.github.com>
2020-12-12 00:33:10 +0800

    Update hfhtml5-ch6

--- hfhtml5/chapter6/mightygumball.js
+++ hfhtml5/chapter6/mightygumball.js
@@ -1,26 +1,42 @@
-window.onload = function () {
+/* mightygumball.js */
+/**
+ * 1. 理解 setInterval
+ * 2. 仍然存在重复数据问题
+ * 3. 有关 Mixed Content 的问题
+ */
+
+var lastReportTime = 0;
+
+window.onload = init;
+
+function init() {
+  console.log('init');
   /* 使用 Ajax 获取数据 */
   // get_data_AJAX()
 
   /* window 可以省略 */
   window.setInterval(handleRefresh, 3000);
+  /* 还不是很懂 setInterval 的机制 */
+  handleRefresh();
 }
+
 function handleRefresh() {
   /** <URL> = https://gumball.wickedlysmart.com/?callback=updateSales_JSONP
-  
   * Use http on GitHub
       Mixed Content: The page at '<URL>' was loaded over HTTPS, but requested an insecure script '<URL>'. 
       This request has been blocked; the content must be served over HTTPS. 
-
-  * Use https on Local
+  * Use https on both
       GET <URL> net::ERR_CERT_COMMON_NAME_INVALID
   */
-  let url = "//gumball.wickedlysmart.com/?callback=updateSales_JSONP";
+  let url = "http://gumball.wickedlysmart.com/"
+    + "?callback=updateSales_JSONP"
+    + "&lastreporttime=" + lastReportTime
+    + "&random=" + (new Date()).getTime();
+  // console.log(url);
   let newScriptElement = document.createElement("script");
   newScriptElement.setAttribute("src", url);
   newScriptElement.setAttribute("id", "jsonp");
   let oldScriptElement = document.getElementById("jsonp");
-
   /* getElementsByTagName 返回的是一个数组，需要指定索引 */
   let head = document.getElementsByTagName("head")[0];
   if (oldScriptElement == null) {
@@ -28,9 +44,11 @@ function handleRefresh() {
   } else {
     head.replaceChild(newScriptElement, oldScriptElement);
   }
+  // console.log(lastReportTime);
 }
 
 function updateSales_JSONP(sales) {
+  // console.log(sales);
   let salesDiv = document.getElementById("sales");
   for (let i = 0; i < sales.length; i++) {
     let sale = sales[i];
@@ -47,13 +65,22 @@ function updateSales_JSONP(sales) {
     } else {
       div.innerHTML = common_innerHTML + " gumballs";
     }
-
     // console.log(div.hasChildNodes());
     /* 可能由于网络问题，不一定每次都能获取到数据 */
     if (div.hasChildNodes()) {
-      salesDiv.appendChild(div);
+      /* 新数据插入到前面 */
+      if (salesDiv.childElementCount == 0) {
+        salesDiv.appendChild(div);
+      }
+      else {
+        salesDiv.insertBefore(div, salesDiv.firstChild);
+      }
     }
     // --------------------------------------------------
+    if (sales.length > 0) {
+      lastReportTime = sales[sales.length - 1].time;
+      // console.log(lastReportTime);
+    }
   }
 }
 
