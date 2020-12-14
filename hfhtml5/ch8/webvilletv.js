93c519f7666809b6cf01b36c3d6fae84b33205b1
fantasy-q <fantasy-q@users.noreply.github.com>
2020-12-14 15:29:35 +0800

    Upadate ch8

---
+++ hfhtml5/ch8/webvilletv.js
@@ -0,0 +1,31 @@
+/* webvilletv.js */
+
+var position = 0;
+var playlist;
+var video;
+
+window.onload = function () {
+  playlist = [
+    "./video/preroll.mp4",
+    "./video/areyoupopular.mp4",
+    "./video/destinationearth.mp4",
+  ];
+  video = document.getElementById("video");
+
+  video.addEventListener("ended", nextVideo, false);
+  video.scr = playlist[position];
+
+  video.load();
+  video.play();
+
+}
+
+function nextVideo() {
+  position++;
+  if (position >= playlist.length) {
+    position = 0;
+  }
+  video.scr = playlist[position];
+  video.load();
+  video.play();
+}
