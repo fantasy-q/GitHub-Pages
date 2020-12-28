2e98481dae1dadb4a62f74969b71f459fd5f9590
fantasy-q <fantasy-q@users.noreply.github.com>
2020-12-29 01:01:01 +0800

    Update css ch5

--- css-mastery/ch5/ch5.js
+++ css-mastery/ch5/ch5.js
@@ -1,7 +1,11 @@
+/* ch5.js */
+
 window.onload = init;
 
 function init() {
-  let ul = document.getElementsByTagName("ul")[0];
+  const ul = document.createElement("ul");
+  const h1 = document.getElementsByTagName("h1")[0];
+  h1.parentNode.appendChild(ul);
 
   for (const key in htmls) {
     if (Object.hasOwnProperty.call(htmls, key)) {
@@ -18,12 +22,25 @@ function init() {
       a.setAttribute("target", "_blank");
       span.setAttribute("class", "right");
       span.innerHTML = description;
-      a.setAttribute("id", filename.slice(3, 5))
+      a.setAttribute("id", "a" + key.padStart(2, 0))
       li.innerHTML = text;
       li.appendChild(span);
       a.appendChild(li);
       ul.appendChild(a);
     }
   }
+  insertHeading();
+}
 
+function insertHeading() {
+  for (const key in titles) {
+    if (Object.hasOwnProperty.call(titles, key)) {
+      const title = titles[key];
+      const heading = document.createElement("h2");
+      const node = document.getElementsByTagName("ul")[0];
+      const id = (key + "").padStart(2, 0);
+      heading.innerHTML = title;
+      node.insertBefore(heading, document.getElementById("a" + id));
+    }
+  }
 }
