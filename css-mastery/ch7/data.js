2035bdb082480c1b85f4e78ec58b17388ba93486
fantasy-q <fantasy-q@users.noreply.github.com>
2021-01-02 23:41:38 +0800

    Create css ch7

---
+++ css-mastery/ch7/data.js
@@ -0,0 +1,25 @@
+const base = './chapter-07';
+
+const title = 'chapter 7 Page Layout and Grids';
+
+const htmls = [
+  { title: 'Planning your Layout', type: 'h2', name: '' },
+  { title: '', type: 'a', name: '07-05-content-wrapper.html' },
+  { title: '', type: 'a', name: '07-07-float-columns-whole-grid.html' },
+  { title: '', type: 'a', name: '07-08-float-columns-grid-and-content.html' },
+  { title: '', type: 'a', name: '07-10-float-columns-grid-fluid-gutters-broken.html' },
+  { title: '', type: 'a', name: '07-11-float-columns-grid-fluid-gutters.html' },
+  { title: '', type: 'a', name: '07-14-float-columns-grid-elastic-gutters-padding.html' },
+  { title: '', type: 'a', name: '07-16-flexbox-columns-equalheight.html' },
+  { title: '', type: 'a', name: '07-22-grid-layout-alignment.html' },
+  { title: '', type: 'a', name: '07-24-grid-layout-auto.html' },
+  { title: '', type: 'a', name: '07-25-grid-layout-order.html' },
+  { title: '', type: 'a', name: '07-26-1-grid-layout-implicit-auto-sparse.html' },
+  { title: '', type: 'a', name: '07-26-2-grid-layout-implicit-auto-dense.html' },
+  { title: '', type: 'a', name: '07-27-grid-layout-template.html' },
+  { title: '', type: 'a', name: '07-29-grid-layout-template-more-ads.html' },
+  { title: '', type: 'a', name: '07-XX-float-columns-grid-fluid-gutters-padding.html' },
+  { title: '', type: 'a', name: '07-XX-float-columns-grid-wrapping-inline.html' },
+  { title: '', type: 'a', name: '07-XX-grid-layout-align-items.html' },
+  { title: '', type: 'a', name: '07-XX-grid-layout-minmax.html' }
+]
