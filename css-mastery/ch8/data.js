cde3e78d644dac9533c3ef4e9ce26b1d6f4b56b7
fantasy-q <fantasy-q@users.noreply.github.com>
2021-01-06 02:23:05 +0800

    Create css ch8

---
+++ css-mastery/ch8/data.js
@@ -0,0 +1,21 @@
+const base = "chapter-08/";
+
+const title = 'chapter 8  Responsive Web Design & CSS';
+
+const htmls = [
+  { title: '响应式：示例', name: '08-05-responsive-news.html' },
+  { title: '', name: '08-10-max-width-typography.html' },
+  { title: '', name: '08-11-multi-column-text.html' },
+  { title: '', name: '08-13-responsive-flexbox-widget.html' },
+  { title: '', name: '08-17-responsive-news-grid.html' },
+  { title: '', name: '08-18-background-image-media-query.html' },
+  { title: '', name: '08-20-responsive-news-srcset-x.html' },
+  { title: '', name: '08-XX-responsive-news-picture.html' },
+  { title: '', name: '08-XX-responsive-news-srcset.html' }
+]
+
+const titles = {
+  05: 'A Responsive Example',
+
+  XX: 'Others'
+}
