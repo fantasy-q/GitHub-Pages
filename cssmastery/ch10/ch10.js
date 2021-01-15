6a7b2b9d6469ebec2dc7c463762b4e3785da14c6
fantasy-q <fantasy-q@users.noreply.github.com>
2021-01-15 12:09:47 +0800

    Update css ch10

--- cssmastery/ch10/ch10.js
+++ cssmastery/ch10/ch10.js
@@ -10,46 +10,46 @@ function init() {
   // 插入到 <ul>
   data.htmls.forEach((element) => {
     const li = createListItem(element);
-    const link = li.parentNode;
     // 插入标题
-    const page = li.firstChild.innerHTML.slice(3, 5);
-    ul.appendChild(insertTitleBefore(page));
-    ul.appendChild(link);
+    const page = li.firstChild.innerText.slice(3, 5);
+    const insertH2 = insertTitleBefore(page);
+    if (insertH2.hasChildNodes()) {
+      ul.appendChild(insertH2);
+    }
+    ul.appendChild(li);
   });
   main.appendChild(ul);
-  console.log(document.head);
 }
 
 function createListItem(element) {
-  const a = document.createElement("a");
   const li = document.createElement("li");
+  const a = document.createElement("a");
   const spanCN = document.createElement("span");
-  const spanEN = document.createElement("span");
   const name = element['name'];
   const path = base + name;
-  li.classList.add("overflow")
+  // 
+  li.setAttribute('data-page',3)
+  console.log(li.dataset);
   // 设置 <a>
   a.href = path;
   a.target = "_blank";
+  a.innerText = name.split('.')[0];
   // 设置中文 <span>
-  spanEN.classList.add("left");
-  spanEN.innerHTML = name.split('.')[0];
   spanCN.classList.add("right");
-  spanCN.innerHTML = element['title']
-  li.appendChild(spanEN);
-  li.appendChild(spanCN);
-  a.appendChild(li);
+  spanCN.innerText = element['title']
+  a.appendChild(spanCN);
+  li.appendChild(a);
   return li;
 }
 
 // Create <title> <h1>
 function createBody() {
   const header = document.createElement("header");
-  document.getElementsByTagName("title")[0].innerHTML = data.chapter;
+  document.getElementsByTagName("title")[0].innerText = data.chapter;
   const body = document.body;
   const main = document.createElement("main");
   const h1 = document.createElement("h1");
-  h1.innerHTML = data.getChapterText();
+  h1.innerText = data.getChapterText();
   header.appendChild(h1);
   body.appendChild(header);
   body.appendChild(main);
@@ -66,7 +66,25 @@ function insertTitleBefore(page) {
     if (Object.hasOwnProperty.call(object, key)) {
       if (page == key) {
         const element = object[key];
-        h2.innerHTML = element;
+        h2.innerText = element;
+        // 防止重复插入
+        delete object[key];
+      }
+    }
+  }
+  return h2;
+}
+// insert item 
+function insertItemBefore(page) {
+  if (page != 'XX') {
+    page = Number.parseInt(page);
+  }
+  const h2 = document.createElement("h2");
+  for (const key in object = data.headings) {
+    if (Object.hasOwnProperty.call(object, key)) {
+      if (page == key) {
+        const element = object[key];
+        h2.innerText = element;
         // 防止重复插入
         delete object[key];
       }
--- cssmastery/ch10/data.js
+++ cssmastery/ch10/data.js
@@ -1,28 +1,32 @@
-const base = "chapter-09/";
+const base = "chapter-10/";
 
 const data = {
-  chapter: 'chapter 10  Styling Forms and Data Tables',
+  chapter: 'chapter 10 Making It Move: Transforms, Transitions, and Animations',
   htmls: [
-    { title: '', name: '10-03-simple-transform.html' },
-    { title: '', name: '10-05-simple-transform-origin.html' },
-    { title: '', name: '10-06-simple-transform-translate.html' },
-    { title: '', name: '10-07-transform-origin-rotate-translate-text.html' },
-    { title: '', name: '10-11-scale-x-text.html' },
+    { title: '变换：旋转', name: '10-03-simple-transform.html' },
+    { title: '变换：原点', name: '10-05-simple-transform-origin.html' },
+    { title: '变换：平移', name: '10-06-simple-transform-translate.html' },
+    { title: '变换', name: '10-07-transform-origin-rotate-translate-text.html' },
+    { title: '放缩', name: '10-11-scale-x-text.html' },
     { title: '', name: '10-12-skew.html' },
-    { title: '', name: '10-13-button-transition.html' },
-    { title: '', name: '10-17-step-animation.html' },
-    { title: '', name: '10-18-transition-to-maxheight.html' },
-    { title: '', name: '10-19-1-box-animation-keyframe-states-illustration.html' },
-    { title: '', name: '10-19-box-animation.html' },
-    { title: '', name: '10-21-animate-along-arc.html' },
-    { title: '', name: '10-24-simple-3d-transform.html' },
-    { title: '', name: '10-25-1-perspective-origin.html' },
-    { title: '', name: '10-25-2-perspective-origin-topleft.html' },
+    { title: '过渡', name: '10-13-button-transition.html' },
+    { title: '动画', name: '10-17-step-animation.html' },
+    { title: '过渡', name: '10-18-transition-to-maxheight.html' },
+    { title: '动画：关键帧', name: '10-19-1-box-animation-keyframe-states-illustration.html' },
+    { title: '动画', name: '10-19-box-animation.html' },
+    { title: '动画', name: '10-21-animate-along-arc.html' },
+    { title: '变换', name: '10-24-simple-3d-transform.html' },
+    { title: '透视', name: '10-25-1-perspective-origin.html' },
+    { title: '透视', name: '10-25-2-perspective-origin-topleft.html' },
     { title: '', name: '10-26-1-flipping-widget-3d-context.html' },
     { title: '', name: '10-26-flipping-widget.html' },
-    { title: '', name: '10-XX-matrix-2d-transform.html' }
+    { title: '变换：二维矩阵', name: '10-XX-matrix-2d-transform.html' }
   ],
+  own: {
+    06: { title: '旋转：直观演示', name: '', path: '../rotate' },
+  },
   headings: {
+    03: '2D Transforms',
     XX: 'Others'
   },
   getChapterText: function () {
